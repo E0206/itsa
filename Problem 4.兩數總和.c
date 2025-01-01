22638ba2fbfe780a30fedfcc954a7a71de659f9a
E0206 <131318394+E0206@users.noreply.github.com>
2025-01-01 17:11:02 +0800

    Create Problem 4.兩數總和.c

---
+++ Problem 4.兩數總和.c
@@ -0,0 +1,15 @@
+//https://etutor2.itsa.org.tw/mod/topics/view.php?id=42595
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int num;
+    scanf("%d", &num);
+    for(int i = 0; i < num; i++){
+        int a, b;
+        scanf("%d%d", &a, &b);
+        printf("%d\n", a + b);
+    }
+	return 0;
+}
