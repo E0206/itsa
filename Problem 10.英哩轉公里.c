b52ea620b01aab0b078dcc4c401075e25f76bfff
E0206 <131318394+E0206@users.noreply.github.com>
2025-01-02 10:21:32 +0800

    Create Problem 10.英哩轉公里.c

---
+++ Problem 10.英哩轉公里.c
@@ -0,0 +1,12 @@
+// https://etutor2.itsa.org.tw/mod/topics/view.php?id=42601
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int mi;
+    scanf("%d", &mi);
+    double km;
+    km = mi * 1.6;
+    printf("km=%.1lf\n", km);
+	return 0;
+}
