bbe34c3043995846d4a07da7c21c28a4b32b2383
E0206 <131318394+E0206@users.noreply.github.com>
2025-01-02 10:24:14 +0800

    Create Problem 12.購票計算.c

---
+++ Problem 12.購票計算.c
@@ -0,0 +1,14 @@
+// https://etutor2.itsa.org.tw/mod/topics/view.php?id=42603
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("NT10=%d\n", n / 10);
+    n = n % 10;
+    printf("NT5=%d\n", n / 5);
+    n = n % 5;
+    printf("NT1=%d\n", n);
+	return 0;
+}
