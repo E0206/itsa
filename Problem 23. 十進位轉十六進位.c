000bc1da76a4891eb8342e18f12369d36e490acb
E0206 <131318394+E0206@users.noreply.github.com>
2025-01-02 10:37:06 +0800

    Create Problem 23. 十進位轉十六進位.c

---
+++ Problem 23. 十進位轉十六進位.c
@@ -0,0 +1,11 @@
+//https://etutor2.itsa.org.tw/mod/topics/view.php?id=42617
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int a;
+    scanf("%d", &a);
+    printf("%X\n", a);
+	  return 0;
+}
